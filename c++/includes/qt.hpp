aa1ef4de0d559a41545c0f996d1b083219c1ce6c
Francisco Contreras <fcocc77@gmail.com>
2020-10-22 12:49:17 -0300

    qt: inset qjasonobject a profundidad

--- c++/includes/qt.hpp
+++ c++/includes/qt.hpp
@@ -12,6 +12,77 @@ namespace qt
     void set_icon(QPushButton *button, QString path);
     void add_widget(QWidget *parent, QWidget *widget);
 
+    template <class T>
+    void insert_json_deep(QJsonObject *object, QStringList keys, T value)
+    {
+        // retorna el ultimo objeto
+        auto get_deep = [=](int deep) {
+            QJsonObject base = *object;
+            for (int i = 0; i < deep; i++)
+            {
+                QString key = keys[i];
+                base = base.value(key).toObject();
+            }
+
+            return base;
+        };
+        //
+        //
+
+        int count = keys.length();
+
+        if (count == 1)
+        {
+            object->insert(keys[0], value);
+        }
+
+        else if (count == 2)
+        {
+            QJsonObject deep0 = get_deep(1);
+            deep0.insert(keys[1], value);
+            object->insert(keys[0], deep0);
+        }
+
+        else if (count == 3)
+        {
+            QJsonObject deep1 = get_deep(2);
+            deep1.insert(keys[2], value);
+
+            QJsonObject deep0 = get_deep(1);
+            deep0.insert(keys[1], deep1);
+            object->insert(keys[0], deep0);
+        }
+
+        else if (count == 4)
+        {
+            QJsonObject deep2 = get_deep(3);
+            deep2.insert(keys[3], value);
+
+            QJsonObject deep1 = get_deep(2);
+            deep1.insert(keys[2], deep2);
+
+            QJsonObject deep0 = get_deep(1);
+            deep0.insert(keys[1], deep1);
+            object->insert(keys[0], deep0);
+        }
+
+        else if (count == 5)
+        {
+            QJsonObject deep3 = get_deep(4);
+            deep3.insert(keys[4], value);
+
+            QJsonObject deep2 = get_deep(3);
+            deep2.insert(keys[3], deep3);
+
+            QJsonObject deep1 = get_deep(2);
+            deep1.insert(keys[2], deep2);
+
+            QJsonObject deep0 = get_deep(1);
+            deep0.insert(keys[1], deep1);
+            object->insert(keys[0], deep0);
+        }
+    }
+
 } // namespace qt
 
 #endif //QT_HPP
